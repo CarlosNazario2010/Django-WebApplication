8eefb63e35aa59ef49ff051dadef81bc0d9816dd
CarlosNazario2010 <95858156+CarlosNazario2010@users.noreply.github.com>
2022-09-02 07:35:46 -0300

    Add files via upload

---
+++ apps/usuarios/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+""" USUARIOS CRIADOS
+
+    carlos@gmail.com            senha = admin       - Superuser
+    carlosnazario@gmail.com     senha = 123456      - Standarduser 
+    
+"""
